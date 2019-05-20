cffeff9e07c9c2fd0ac95bfe3a7a849dc50b0269
xiaokun.wang <493501331@qq.com>
2019-05-20 08:43:07 +0800

    放开手机及平板访问限制

--- src/main/java/com/thinkgem/jeesite/modules/sys/interceptor/MobileInterceptor.java
+++ src/main/java/com/thinkgem/jeesite/modules/sys/interceptor/MobileInterceptor.java
@@ -29,12 +29,12 @@ public class MobileInterceptor extends BaseService implements HandlerInterceptor
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, 
 			ModelAndView modelAndView) throws Exception {
-		if (modelAndView != null){
+		/*if (modelAndView != null){
 			// 如果是手机或平板访问的话，则跳转到手机视图页面。
 			if(UserAgentUtils.isMobileOrTablet(request) && !StringUtils.startsWithIgnoreCase(modelAndView.getViewName(), "redirect:")){
 				modelAndView.setViewName("mobile/" + modelAndView.getViewName());
 			}
-		}
+		}*/
 	}
 
 	@Override
